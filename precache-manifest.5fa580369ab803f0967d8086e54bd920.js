243c4ac750de8d7dc6469480aa908e146422bb39
Kashishdwn <kashishdwn@gmail.com>
2020-04-24 01:10:08 +0530

    Updates

---
+++ precache-manifest.5fa580369ab803f0967d8086e54bd920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c1b3a43ca2021868a0122659c7cfce",
+    "url": "/Coin-Flip/index.html"
+  },
+  {
+    "revision": "ae6d45b20c4d369d63ae",
+    "url": "/Coin-Flip/static/css/main.cf776fa5.chunk.css"
+  },
+  {
+    "revision": "094318b5c2be2a8c091b",
+    "url": "/Coin-Flip/static/js/2.13bb3cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Coin-Flip/static/js/2.13bb3cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6d45b20c4d369d63ae",
+    "url": "/Coin-Flip/static/js/main.d02ee846.chunk.js"
+  },
+  {
+    "revision": "5c1cffb33467cf22a13e",
+    "url": "/Coin-Flip/static/js/runtime-main.9605ab79.js"
+  }
+]);
